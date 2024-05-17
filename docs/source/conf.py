f85ba2b67842d3a7ee307c9a3d4935a8db11f6be
ScottAhern <scottahern96@gmail.com>
2024-05-17 07:33:05 +0000

    feat: sphinx directory populated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,28 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "example_project"
-copyright = "2024, ScottAhern"
-author = "ScottAhern"
-release = "1.2.0"
+project = 'example_project'
+copyright = '2024, ScottAhern'
+author = 'ScottAhern'
+release = '1.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.autodoc",  # Allows sphinx-apidoc to work
-    "sphinx.ext.todo",  # optional.  Allows inline "todo:"
-    "sphinx.ext.imgmath",  # optional. Allows LaTeX equations
-    "sphinx.ext.napoleon",  # Allows google/numpy docstrings
-    "sphinx.ext.githubpages",  # Adds .nojekyll file
-]
+extensions = []
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = 'alabaster'
+html_static_path = ['_static']
